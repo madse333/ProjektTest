243c504edb117736323c6084e83d71fbd5bae654
Mads Bjerg <au566699@uni.au.dk>
2022-02-23 10:16:26 +0100

    lmao

--- Projekt test/src/ordination/DagligFast.java
+++ Projekt test/src/ordination/DagligFast.java
@@ -1,5 +1,19 @@
 package ordination;
 
+import java.time.LocalTime;
+import java.util.ArrayList;
+
 public class DagligFast {
     // TODO
+    private final Dosis[] doser = new Dosis[4];
+    private int antalDoser = 0;
+
+    public ArrayList<Dosis> getDoser(){
+        return new ArrayList<>(doser);
+    }
+
+    public void opretDosis(LocalTime tid, double antal){
+        doser[antalDoser] = new Dosis(tid,antal);
+        antalDoser++;
+    }
 }
