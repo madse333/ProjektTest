80383e0f7b20160993dbc9e96fb795fb51e37eef
madse333 <madsen170@yahoo.dk>
2022-02-23 10:03:14 +0100

    Halvfærdig dagligFast-klasse

--- Projekt test/src/ordination/DagligFast.java
+++ Projekt test/src/ordination/DagligFast.java
@@ -1,5 +1,38 @@
 package ordination;
 
-public class DagligFast {
-    // TODO
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class DagligFast extends Ordination {
+
+    //Link attribut
+    private final Dosis[] doser = new Dosis[4];
+
+    public DagligFast(LocalDate startDen, LocalDate slutDen){
+        super(startDen,slutDen);
+
+    }
+    public Dosis[] getDoser(){return doser;}
+
+    public Dosis createDosis(LocalTime tid, double antal){
+        Dosis dosis = new Dosis(tid,antal);
+
+        return dosis;
+    }
+
+    @Override
+    public double samletDosis() {
+        return 0;
+    }
+
+    @Override
+    public double doegnDosis() {
+        return 0;
+    }
+
+    @Override
+    public String getType() {
+        return null;
+    }
+
 }
