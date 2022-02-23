500befafb6ed863b6ed4eb320b53608c84dc42b6
Esben Sommer Bach <eaaesbac@students.eaaa.dk>
2022-02-23 12:41:04 +0100

    DagligSkaev- Fjernet TODO

--- Projekt test/src/ordination/DagligSkaev.java
+++ Projekt test/src/ordination/DagligSkaev.java
@@ -6,7 +6,6 @@ import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 
 public class DagligSkaev extends Ordination {
-    // TODO
     // composition --> 0..* Dosis
     private final ArrayList<Dosis> doser = new ArrayList<>();
 
