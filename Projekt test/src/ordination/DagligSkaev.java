531be189087d9cd4ac310e6baea4e6cd134ec32d
Esben Sommer Bach <eaaesbac@students.eaaa.dk>
2022-02-23 10:48:17 +0100

    DagligSkaev ændringer

--- Projekt test/src/ordination/DagligSkaev.java
+++ Projekt test/src/ordination/DagligSkaev.java
@@ -1,9 +1,10 @@
 package ordination;
 
+import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
 
-public class DagligSkaev {
+public class DagligSkaev extends Ordination {
     // TODO
     // composition --> 0..* Dosis
     private final ArrayList<Dosis> doser = new ArrayList<>();
@@ -12,9 +13,29 @@ public class DagligSkaev {
         return new ArrayList<>(doser);
     }
 
+    public DagligSkaev(LocalDate startDen, LocalDate slutDen, Patient patient, Laegemiddel laegemiddel,
+                       LocalTime[] klokkeSlet, double[] antalEnheder) {
+        super(startDen,slutDen,laegemiddel);
+    }
+
     public void opretDosis(LocalTime tid, double antal) {
         // TODO
         Dosis dosis = new Dosis(tid,antal);
         doser.add(dosis);
+    }
+
+    @Override
+    public double samletDosis() {
+        return 0;
+    }
+
+    @Override
+    public double doegnDosis() {
+        return 0;
+    }
+
+    @Override
+    public String getType() {
+        return null;
     }
 }
