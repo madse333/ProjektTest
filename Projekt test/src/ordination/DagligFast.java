4e1293f66fea3850cfd52f701cad7119754236c4
madse333 <madsen170@yahoo.dk>
2022-02-23 11:06:02 +0100

    DagligFast opdateret

--- Projekt test/src/ordination/DagligFast.java
+++ Projekt test/src/ordination/DagligFast.java
@@ -1,6 +1,8 @@
 package ordination;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+import java.util.concurrent.ForkJoinPool;
 
 public class DagligFast extends Ordination {
 
@@ -8,33 +10,48 @@ public class DagligFast extends Ordination {
     private final Dosis[] doser = new Dosis[4];
     private int antalDoser = 0;
 
-    public DagligFast(LocalDate startDen, LocalDate slutDen, Laegemiddel laegemiddel){
+    public DagligFast(LocalDate startDen, LocalDate slutDen, Laegemiddel laegemiddel, double morgenAntal,
+                      double middagsAntal, double aftenAntal, double natAntal){
         super(startDen,slutDen, laegemiddel);
+        doser[0] = new Dosis(LocalTime.of(8,0),morgenAntal);
+        doser[1] = new Dosis(LocalTime.of(12,0),middagsAntal);
+        doser[2] = new Dosis(LocalTime.of(18,0),aftenAntal);
+        doser[3] = new Dosis(LocalTime.of(0,0),natAntal);
     }
+
     public Dosis[] getDoser(){return doser;}
 
-    public void createDosis(LocalTime tid, double antal){
-        try{
-            Dosis dosis = new Dosis(tid,antal);
-            doser[antalDoser] = dosis;
-            antalDoser++;
-        }catch(ArrayIndexOutOfBoundsException ex){
-            System.out.println("Exception: " + ex.getMessage() + " sikkert for mange doser tilføjet.");
-        }
-    }
+//    public void createDosis(LocalTime tid, double antal){
+//        try{
+//            Dosis dosis = new Dosis(tid,antal);
+//            doser[antalDoser] = dosis;
+//            antalDoser++;
+//        }catch(ArrayIndexOutOfBoundsException ex){
+//            System.out.println("Exception: " + ex.getMessage() + " sikkert for mange doser tilføjet.");
+//        }
+//    }
 
     @Override
     public double samletDosis() {
-        return 0;
+        double samletdosis = 0;
+        long days = ChronoUnit.DAYS.between(this.getStartDen(),this.getSlutDen());
+        for (Dosis d : doser){
+            samletdosis += d.getAntal();
+        }
+        return samletdosis * days;
     }
 
     @Override
     public double doegnDosis() {
-        return 0;
+        double samletdosis = 0;
+        for (Dosis d : doser){
+            samletdosis += d.getAntal();
+        }
+        return samletdosis;
     }
 
     @Override
     public String getType() {
-        return null;
+        return "Daglig Fast";
     }
 }
