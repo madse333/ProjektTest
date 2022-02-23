69a5af41658c6f0c774c1e9507294bd7a1143dcf
madse333 <madsen170@yahoo.dk>
2022-02-23 09:28:08 +0100

    Linkatribut med metoder fra patient til ordination

--- Projekt test/src/ordination/Patient.java
+++ Projekt test/src/ordination/Patient.java
@@ -1,11 +1,14 @@
 package ordination;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Patient {
     private String cprnr;
     private String navn;
     private double vaegt;
 
-    // TODO: Link til Ordination
+    private List<Ordination> ordinationer = new ArrayList<>();
 
     public Patient(String cprnr, String navn, double vaegt) {
         this.cprnr = cprnr;
@@ -33,7 +36,15 @@ public class Patient {
         this.vaegt = vaegt;
     }
 
-    //TODO: Metoder (med specifikation) til at vedligeholde link til Ordination
+    public List<Ordination> getOrdinationer(){return new ArrayList<>(ordinationer);}
+
+    public void addOrdination(Ordination ordination){
+        ordinationer.add(ordination);
+    }
+
+    public void removeOrdination(Ordination ordination){
+        ordinationer.remove(ordination);
+    }
 
     @Override
     public String toString(){
