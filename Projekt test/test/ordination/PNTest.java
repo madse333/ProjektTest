d5f8eb5cdb409dfdc4f588bf534ba278b4af6453
Esben Sommer Bach <eaaesbac@students.eaaa.dk>
2022-02-25 10:54:50 +0100

    PN. Ændringer på givDosis PNTest startet

--- Projekt test/test/ordination/PNTest.java
+++ Projekt test/test/ordination/PNTest.java
@@ -8,7 +8,22 @@ class PNTest {
 
     @org.junit.jupiter.api.Test
     void givDosis() {
+        // Arrange
+        Patient patient1 = new Patient("050972-1233", "Hans Jørgensen", 89.4);
 
+        Laegemiddel laegemiddel = new Laegemiddel("Acetylsalicylsyre",0.5,1,
+                2,"Stuk");
+
+        PN pn = new PN(LocalDate.of(2010,2,1),LocalDate.of(2010,2,10),
+                2);
+
+        // Act
+        boolean TC1 = pn.givDosis(LocalDate.of(2010,2,1));
+        boolean TC2 = pn.givDosis(LocalDate.of(2010,2,10));
+
+        // Assert
+        assertTrue(TC1);
+        assertTrue(TC2);
     }
 
 
